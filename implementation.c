c25f6b5545278348d42a2a41821fe44c4229c1f2
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-02-02 20:46:32 -0600

    Update implementation.c

--- implementation.c
+++ implementation.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
+#include "TestDrivenRanges.h"
+#include <assert.h>
+
+char* Response(int a, int b)
+{
+    char *response = malloc(10);
+    strcpy(response, "4-5, 2");
+    return response;
+}
+
 
 int main()
 {
-
+  
+assert(strcmp(Response(4,5),"4-5, 2") == 0);
 
 return 0;
 }
