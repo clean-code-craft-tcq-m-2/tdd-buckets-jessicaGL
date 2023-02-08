295aa6ecb292d1b6cb25c0a19a31a7424706001d
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-02-07 21:26:28 -0600

    Update TestDrivenRanges.h
    draft of the header

--- TestDrivenRanges.h
+++ TestDrivenRanges.h
@@ -3,3 +3,7 @@
 #include <string.h>
 
 char* Response(int max, int min);
+void swap(int* xp, int* yp);
+void printArray(int arr[], int size);
+void sortarray(int arr[], int n);
+void defineranges(int arr[], int n);
