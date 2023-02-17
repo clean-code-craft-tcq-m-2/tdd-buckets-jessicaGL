ba4be827557e757f04f812d0beb79a78c14e68de
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-02-16 22:06:18 -0600

    Update TestDrivenRanges.h
    adding assert headers

--- TestDrivenRanges.h
+++ TestDrivenRanges.h
@@ -7,3 +7,6 @@ void swap(int* xp, int* yp);
 void printArray(int arr[], int size);
 void sortarray(int arr[], int n);
 void defineranges(int arr[], int n);
+char *Rangeformat_Test(int low, int up,int count);
+int RangeCount_Test(int input[], int n, int low, int up);
+bool ArraySorted_test(int input[], int inputsorted[], int n);
