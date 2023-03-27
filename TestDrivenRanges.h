2692f31e66c2bf352a6366fc1dd156ade498919f
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-03-26 22:26:13 -0600

    Update TestDrivenRanges.h
    Update the library to adding the new output function

--- TestDrivenRanges.h
+++ TestDrivenRanges.h
@@ -2,11 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* Response(int max, int min);
 void swap(int* xp, int* yp);
 void printArray(int arr[], int size);
 void sortarray(int arr[], int n);
 void defineranges(int arr[], int n);
-char *Rangeformat_Test(int low, int up,int count);
-int RangeCount_Test(int input[], int n, int low, int up);
-bool ArraySorted_test(int input[], int inputsorted[], int n);
+void Output(int StartofRange[], int EndofRange[], int CountinRange[], int size);
