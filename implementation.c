820f20fc199b22d71957d9595cc9840b51b96a43
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-03-26 22:23:51 -0600

    Update implementation.c
    Adding a function to print the output to the console.

--- implementation.c
+++ implementation.c
@@ -36,102 +36,66 @@ void sortarray(int arr[], int n)
     printArray(arr,n);
 }
 
-
 void defineranges(int arr[], int n)
 {
-    int lowlimit[2], uplimit[2],rangecount[2],count = 1, i, limit_index = 0;
-    lowlimit[0] = arr[0];
-    for(i = 1; i < n; i++)
-    {
-        count++;
-        printf("\nel valor de i es: %d\n", i);
-        if((arr[i+1] - arr[i]) > 1)
+    int start = arr[0];
+    int end = arr[0];
+    int range_count = 0;
+    int readings = 1;
+
+    int start_array[n], end_array[n], read_array[n];
+
+      for (int i = 1; i < n; i++)
+      {
+        if (arr[i] - arr[i-1] > 1)
         {
-            uplimit[limit_index] = arr[i];
-            printf("un rango: %d \n", arr[i]);
-            rangecount[limit_index] = count;
-            limit_index++;
-            count = 0;
-            lowlimit[limit_index] = arr[i+1];
-        }
-        if(i == n-1)
+          start_array[range_count] = start;
+          end_array[range_count] = end;
+          read_array[range_count] = readings;
+          readings = 1;
+          
+          range_count++;
+          
+    
+          start = arr[i];
+          end = arr[i];
+        }else 
         {
-            uplimit[limit_index] = arr[i];
-            rangecount[limit_index] = count;
+          end = arr[i];
+          readings++;
         }
-    }
- 
-   printArray(lowlimit, 2); 
-   printArray(uplimit,2);
-   printArray(rangecount,2);
+      }
+    
+      start_array[range_count] = start;
+      end_array[range_count] = end;
+      read_array[range_count] = readings;
+      range_count++;
+    
+      Output(start_array,end_array,read_array, range_count);
+
 }
 
-
-char* Response(int a, int b)
+void Output(int StartofRange[], int EndofRange[], int CountinRange[], int size)
 {
-    char *response = (char*)malloc(10);
-    strcpy(response, "4-5, 2");
-    return response;
-}
-
-bool ArraySorted_test(int input[], int inputsorted[], int n)
-{
-    bool sorted = true;
-    int i;
-    for(i = 0 ; i < n ; i++)
+    printf("Range, Readings\n");
+    for (int i = 0; i < size; i++) 
     {
-       if(input[i] != inputsorted[i])
-       {
-           sorted = false;
-       }
+        printf("%d-%d, %d\n", StartofRange[i], EndofRange[i], CountinRange[i]); 
     }
     
-    return sorted;
 }
-
-int RangeCount_Test(int input[], int n, int low, int up)
-{
-    int count= 0, i;
-    for(i = 0; i < n ; i++)
-    {
-        if(input[i]>=low && input[i]<= up)
-        {
-            count++;
-        }
-    }
-    
-    return count;
-}
-
-char *Rangeformat_Test(int low, int up,int count)
-{
-    char string[20];
-    
-    sprintf(string,"%d-%d, %d",low, up, count);
-    return string;
-}
-
 
 int main()
 {
   
-    assert(strcmp(Response(4,5),"4-5, 2") == 0);
-    
-    
     int input[5] = {8,4,2,3,7};
-    int n = 0;
-    n = sizeof(input)/sizeof(input[0]);
+    int n = (sizeof(input)/sizeof(input[0]));
     sortarray(input, n);
     defineranges(input, n);
-    int Input[10] = {8,15,9,2,7,6,9,1,25,0};
-    int sortedarray[10] = {0,1,2,6,7,8,9,9,15,25};
-    int n = sizeof(Input)/sizeof(Input[0]);
-    sortarray(Input,n);
-    //test the sort array function
-    assert(ArraySorted_test(Input,sortedarray,n) == true );
-    //test the ranges
-    assert(RangeCount_Test(Input,n,6,9) == 5);
-    //assert(strcmp(Rangeformat_Test(6,9),"6-9, 5")= 0)
+    int Example2[10] = {8,15,9,2,7,6,9,1,25,0};
+    int m = (sizeof(Example2)/sizeof(Example2[0]));
+    sortarray(Example2,m);
+    defineranges(Example2,m);
     
     return 0;
 }
